441e9b12fadadea912fc22416b0d13529651bbcd
Walter <wtl4112@rit.edu>
2014-02-24 13:36:09 -0500

    Making progress on the APIC.
    Now the APIC is enabled and appears to work. To test, interrupts have to
    be enabled by added an __asm__("sti") after apic_init() is called. Will
    be added later.
    
    Currently the APIC is being used in compatability mode with the old 8259
    PIC. The next work will be to find and enable I/O APICs instead of the
    8259.

--- kernel/x86_64/src/interrupts/apic.c
+++ kernel/x86_64/src/interrupts/apic.c
@@ -7,8 +7,38 @@
 #include "safety.h"
 #include "kprintf.h"
 #include "inttypes.h"
+#include "defines.h"
+#include "support.h"
+
+// For the 8259 PIC
+#define PIC_MASTER_CMD_PORT 0x20
+#define PIC_MASTER_IMR_PORT 0x21
+#define PIC_SLAVE_CMD_PORT 0xA0
+#define PIC_SLAVE_IMR_PORT 0xA1
+#define PIC_MASTER_SLAVE_LINE 0x04
+#define PIC_SLAVE_ID 0x02
+#define PIC_86MODE 0x1
+#define PIC_ICW1BASE 0x10
+#define PIC_NEEDICW4 0x01
+#define PIC_EOI 0x20
+
+#define PIC_REMAP_BASE 0x30
 
 #define APIC_BASE_MSR 0x1B
+#define APIC_EN (0x10)
+#define LVT_MASK (0x1 << 16)
+#define LVT_EXTINT (0x700)
+#define LVT_NMI (0x400)
+#define LVT_LEVEL_TRIG (0x8000)
+#define EOI_IDX (0xB0/sizeof(uint32_t))
+#define TIMER_IDX (0x320 / sizeof(uint32_t))
+#define LINT0_IDX (0x350 / sizeof(uint32_t))
+#define LINT1_IDX (0x360 / sizeof(uint32_t))
+#define PERFCNT_IDX (0x340 / sizeof(uint32_t))
+#define ERROR_IDX (0x370 / sizeof(uint32_t))
+#define SPURIOUS_IDX (0xF0 / sizeof(uint32_t))
+
+#define SPURIOUS_IRQ 0x50
 
 #define APIC_PRESENCE (0x1 << 9)
 #define APIC_LOCATION 0xFFFFFFFFFFFFF000
@@ -25,8 +55,65 @@ uint32_t apic_base_address()
 	return eax & 0xFFFFF000;
 }
 
+static
+void redirect_pic()
+{
+	// ICW1
+	_outb(PIC_MASTER_CMD_PORT, PIC_ICW1BASE | PIC_NEEDICW4);		
+	_outb(PIC_SLAVE_CMD_PORT, PIC_ICW1BASE | PIC_NEEDICW4);
+
+	// ICW2
+	// Master offset to 32
+	// Slave offset to 40
+	_outb(PIC_MASTER_IMR_PORT, PIC_REMAP_BASE);
+	_outb(PIC_SLAVE_IMR_PORT, PIC_REMAP_BASE+0x8);
+
+	// ICW3
+	_outb(PIC_MASTER_IMR_PORT, PIC_MASTER_SLAVE_LINE);
+	_outb(PIC_SLAVE_IMR_PORT, PIC_SLAVE_ID);
+
+	// ICW4
+	_outb(PIC_MASTER_IMR_PORT, PIC_86MODE);
+	_outb(PIC_SLAVE_IMR_PORT, PIC_86MODE);
+
+	// OCW1 Allow interrupts on all lines
+	_outb(PIC_MASTER_IMR_PORT, 0x0);
+	_outb(PIC_SLAVE_IMR_PORT, 0x0);
+}
+
+static
+void apic_spurious_handler(uint64_t vector, uint64_t code)
+{
+	kprintf("Spurious: 0x%x - %d\n", vector, code);
+
+	// According to section 10.9 of the Intel manual the spurious
+	// interrupt handler should return without an EOI.
+}
+
+inline __attribute__((always_inline))
+void pic_acknowledge(const uint64_t vector)
+{
+	if (vector >= 0x30 && vector < 0x40)
+	{
+		_outb(PIC_MASTER_CMD_PORT, PIC_EOI);
+		if (vector > 0x38)
+		{
+			_outb(PIC_SLAVE_CMD_PORT, PIC_EOI);
+		}
+	}
+}
+
+inline __attribute__((always_inline))
+void apic_eoi()
+{
+	APIC_REGS[EOI_IDX] = 0;
+}
+
 void apic_init()
 {
+	// Redirect the PIC
+	redirect_pic();
+
 	// By default the LAPIC is mapped to the address
 	// 0xFEE00000 and should have caching disabled.
 	// Don't know if this is sufficient, but seems to work.
@@ -63,4 +150,21 @@ void apic_init()
 	// Try to read the APIC ID from the newly mapped address
 	kprintf("APIC ID: 0x%x\n", APIC_REGS[0x20/4]);	
 	kprintf("APIC VER: 0x%x\n", APIC_REGS[0x30/4]);	
+
+	// Disable the timer interrupt
+	APIC_REGS[TIMER_IDX] = LVT_MASK;
+	// When delivery mode is EXTINT it's always level triggered
+	APIC_REGS[LINT0_IDX] = LVT_LEVEL_TRIG | LVT_EXTINT;
+	// Vector information is ignored with NMI setting
+	APIC_REGS[LINT1_IDX] = LVT_NMI;
+	APIC_REGS[PERFCNT_IDX] = LVT_MASK;
+	APIC_REGS[ERROR_IDX] = LVT_MASK;
+	// Set the spurious register while also enabling the APIC
+	APIC_REGS[SPURIOUS_IDX] = SPURIOUS_IRQ | APIC_EN;
+
+	APIC_REGS[LINT0_IDX] = LVT_LEVEL_TRIG | LVT_EXTINT;
+	APIC_REGS[LINT1_IDX] = LVT_NMI;
+
+	// Install spurious handler
+	interrupts_install_isr(SPURIOUS_IRQ, apic_spurious_handler);
 }
--- kernel/x86_64/src/interrupts/apic.h
+++ kernel/x86_64/src/interrupts/apic.h
@@ -1,6 +1,8 @@
 #ifndef __X86_64_APIC_H__
 #define __X86_64_APIC_H__
 
+/* Setup the LAPIC if it exists.
+ */
 void apic_init(void);
 
 #endif
