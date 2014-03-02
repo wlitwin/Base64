12bd51f3ff0624c4e961f6d902721a04e5e75e29
Walter <wtl4112@rit.edu>
2014-03-02 15:31:24 -0500

    Closer to finishing the parsing of the MP tables
    Lots of useful information is contained in the MP tables. It includes
    the number of processors, I/O APIC locations, APIC locations, processor
    APIC ids, processor feature flags... All kinds of stuff. Right now the
    tables aren't being used for anything, but they are recognized.
    
    More work needs to be done for enabling the I/O APIC and turning the
    interrupt mappings into something useful.

--- kernel/x86_64/src/interrupts/ioapic.c
+++ kernel/x86_64/src/interrupts/ioapic.c
@@ -53,8 +53,195 @@ COMPILE_ASSERT(sizeof(MPConfigTable) == 44);
 // http://download.intel.com/design/pentium/datashts/24201606.pdf
 #define IOAPIC1_BASE 0xFEC00000
 
+const MFPStruct* mfps;
+
+static
+MFPStruct* find_mfp_struct(void);
+
+static
+void parse_mct(const MPConfigTable* mct);
 
 void ioapic_init()
+{
+	mfps = find_mfp_struct();
+
+	if (mfps == NULL)
+	{
+		panic("Could not find MP Signature");
+	}
+
+	kprintf("Found MP Sig: 0x%x\n", mfps);
+	kprintf("Sig: 0x%x\n", mfps->signature);
+	kprintf("PA Ptr: 0x%x\n", mfps->phys_addr_ptr);
+	kprintf("Len: %d\n", mfps->length);
+	kprintf("Spec Rev: %d\n", mfps->spec_rev);
+	kprintf("Checksum: 0x%x\n", mfps->checksum);
+	kprintf("Feature Info 1: 0x%x\n", mfps->feature_info1);
+	kprintf("Feature Info 2: 0x%x\n", mfps->feature_info2);
+
+	// If the phys_addr_ptr field is non-zero then there is a 
+	// configuration table present. Also if the feature_info1
+	// field is zero then an MP configuration table is present.
+	if (mfps->phys_addr_ptr != 0)
+	{
+		MPConfigTable* mct = (MPConfigTable*) (uint64_t) mfps->phys_addr_ptr;
+		kprintf("\n\nSig: 0x%x\n", mct->signature);
+		kprintf("Tbl Len: %d\n", mct->base_length);
+		kprintf("Spec Rev: %d\n", mct->spec_rev);
+		kprintf("Checksum: 0x%x\n", mct->checksum);
+		kprintf("OEM ID: %l8s\n", mct->oem_id);
+		kprintf("PROD ID: %l12s\n", mct->product_id);
+		kprintf("OEM Tbl Ptr: 0x%x\n", mct->oem_table_ptr);
+		kprintf("OEM Tbl Size: %d\n", mct->oem_table_size);
+		kprintf("Entry Count: %d\n", mct->entry_count);
+		kprintf("Apic Loc: 0x%x\n", mct->lapic_addr);
+		kprintf("Ext Tbl Len: %d\n", mct->ext_table_length);
+		kprintf("Ext Tbl Check: %d\n", mct->ext_table_checksum);
+
+		parse_mct(mct);
+	}
+}
+
+/* Parse the entries of a Multi-Processor Configuration Table. Entries
+ * follow the table header and are of variable length. The entry_count
+ * field of the MPConfigTable structure denotes how many entries to
+ * parse. The first byte of each entry defines the entry type. All
+ * entries are in ascending order sorted on the entry type.
+ *
+ * See Page 4-6 (42) in
+ * http://download.intel.com/design/pentium/datashts/24201606.pdf
+ *
+ * Currently there are 5 defined entry types:
+ *
+ *   Type 0 - Processor - 20 bytes - One entry for every processor
+ *   Type 1 - Bus       -  8 bytes - One entry per bus
+ *   Type 2 - I/O APIC  -  8 bytes - One entry per I/O APIC
+ *   Type 3 - I/O Interrupt Assignment - 8 bytes - One per interrupt source
+ *   Type 4 - Local Interrupt Assignment - 8 bytes - One per system interrupt source
+ */
+#define PROC_ENT_TYPE   0
+#define BUS_ENT_TYPE    1
+#define IOAPIC_ENT_TYPE 2
+#define IOINT_ENT_TYPE  3
+#define LAPIC_ENT_TYPE  4
+
+typedef struct
+{
+	uint8_t entry_type;
+	uint8_t lapic_id;
+	uint8_t lapic_ver;
+	uint8_t cpu_flags;
+	uint32_t cpu_signature;
+	uint32_t feature_flags;
+	uint32_t reserved[2];
+} ProcEntry;
+
+COMPILE_ASSERT(sizeof(ProcEntry) == 20);
+
+typedef struct
+{
+	uint8_t entry_type;
+	uint8_t bus_id;
+	char bus_type_str[6];
+} BusEntry;
+
+COMPILE_ASSERT(sizeof(BusEntry) == 8);
+
+typedef struct
+{
+	uint8_t entry_type;
+	uint8_t id;
+	uint8_t version;
+	uint8_t flags;
+	uint32_t address;
+} IOAPICEntry;
+
+COMPILE_ASSERT(sizeof(IOAPICEntry) == 8);
+
+typedef struct
+{
+	uint8_t entry_type;
+	uint8_t interrupt_type;
+	uint8_t flags;
+	uint8_t reserved;
+	uint8_t src_bus_id;
+	uint8_t src_bus_irq;
+	uint8_t dst_ioapic_id;
+	uint8_t dst_ioapic_int;
+} IOIntEntry;
+
+COMPILE_ASSERT(sizeof(IOIntEntry) == 8);
+
+typedef struct
+{
+	uint8_t entry_type;
+	uint8_t int_type;
+	uint8_t flags;
+	uint8_t reserved;
+	uint8_t src_bus_id;
+	uint8_t src_bus_irq;
+	uint8_t dst_lapic_id;
+	uint8_t dst_lapic_int;
+} LAPICIntEntry;
+
+COMPILE_ASSERT(sizeof(LAPICIntEntry) == 8);
+
+static
+void parse_mct(const MPConfigTable* mct)
+{
+	const uint16_t entries = mct->entry_count;	
+	const uint8_t* type_ptr = (uint8_t*) (mct + 1);
+	for (uint32_t i = 0; i < entries; ++i)
+	{
+		kprintf("Entry: %d - Type: %d\n", i, *type_ptr);
+		switch (*type_ptr)
+		{
+			case PROC_ENT_TYPE:
+				{
+					kprintf("Processor\n");
+					type_ptr += sizeof(ProcEntry);
+				}
+				break;
+			case BUS_ENT_TYPE:
+				{
+					type_ptr += sizeof(BusEntry);
+				}
+				break;
+			case IOAPIC_ENT_TYPE:
+				{
+					type_ptr += sizeof(IOAPICEntry);
+				}
+				break;
+			case IOINT_ENT_TYPE:
+				{
+					type_ptr += sizeof(IOIntEntry);
+				}
+				break;
+			case LAPIC_ENT_TYPE:
+				{
+					type_ptr += sizeof(LAPICIntEntry);
+				}
+				break;
+			default:
+				panic("Unhandled case");
+				break;
+		}
+	}
+}
+
+/* The Multi-Processor Floating Pointer Structure contains information
+ * about the multi-processor configuration of the system. It is used
+ * (among other things) to find the I/O APICs. The first I/O APIC default
+ * location is 0xFEC00000, but there may be more than one or it may
+ * have been moved, so it's best to double check.
+ *
+ * This function goes through the four possible locations of the MFPS
+ * and searches for the _MP_ signature. It will return the location of
+ * the MFPS if found, or NULL if none of the locations contained the
+ * signature.
+ */
+static
+MFPStruct* find_mfp_struct()
 {
 	// First region to search is the first kilobyte 
 	// of the Extended BIOS Data Area (EDBA). But first
@@ -73,8 +260,7 @@ void ioapic_init()
 	{
 		if (edba[i] == MP_SIG)
 		{
-			mfp = (MFPStruct*)&edba[i];
-			break;
+			return (MFPStruct*)&edba[i];
 		}
 	}
 
@@ -97,43 +283,10 @@ void ioapic_init()
 		{
 			if (read_only[i] == MP_SIG)
 			{
-				mfp = (MFPStruct*)&read_only[i];
-				break;
+				return (MFPStruct*)&read_only[i];
 			}
 		}
 	}
 
-	if (mfp == NULL)
-	{
-		panic("Could not find MP Signature");
-	}
-
-	kprintf("Found MP Sig: 0x%x\n", mfp);
-	kprintf("Sig: 0x%x\n", mfp->signature);
-	kprintf("PA Ptr: 0x%x\n", mfp->phys_addr_ptr);
-	kprintf("Len: %d\n", mfp->length);
-	kprintf("Spec Rev: %d\n", mfp->spec_rev);
-	kprintf("Checksum: 0x%x\n", mfp->checksum);
-	kprintf("Feature Info 1: 0x%x\n", mfp->feature_info1);
-	kprintf("Feature Info 2: 0x%x\n", mfp->feature_info2);
-
-	// If the phys_addr_ptr field is non-zero then there is a 
-	// configuration table present. Also if the feature_info1
-	// field is zero then an MP configuration table is present.
-	if (mfp->phys_addr_ptr != 0)
-	{
-		MPConfigTable* mct = (MPConfigTable*) (uint64_t) mfp->phys_addr_ptr;
-		kprintf("\n\nSig: 0x%x\n", mct->signature);
-		kprintf("Tbl Len: %d\n", mct->base_length);
-		kprintf("Spec Rev: %d\n", mct->spec_rev);
-		kprintf("Checksum: 0x%x\n", mct->checksum);
-		kprintf("OEM ID: %l8s\n", mct->oem_id);
-		kprintf("PROD ID: %l12s\n", mct->product_id);
-		kprintf("OEM Tbl Ptr: 0x%x\n", mct->oem_table_ptr);
-		kprintf("OEM Tbl Size: %d\n", mct->oem_table_size);
-		kprintf("Entry Count: %d\n", mct->entry_count);
-		kprintf("Apic Loc: 0x%x\n", mct->lapic_addr);
-		kprintf("Ext Tbl Len: %d\n", mct->ext_table_length);
-		kprintf("Ext Tbl Check: %d\n", mct->ext_table_checksum);
-	}
+	return NULL;
 }
