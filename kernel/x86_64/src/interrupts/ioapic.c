3a89d168e446bbf52d9b02a9880335704dd5e6d3
Walter <wtl4112@rit.edu>
2014-03-15 17:54:31 -0400

    Removed mptables functionality, added ioapic_read/write
    Added functions for reading and writing to I/O APICs.

--- kernel/x86_64/src/interrupts/ioapic.c
+++ kernel/x86_64/src/interrupts/ioapic.c
@@ -17,45 +17,68 @@
 
 const MFPStruct* mfps;
 
-
 void ioapic_init()
 {
-	mfps = find_mfp_struct();
+	// Map the I/O APICs to a virtual address below the LAPIC
+}
 
-	if (mfps == NULL)
-	{
-		panic("Could not find MP Signature");
-	}
+/* All reads/writes must be done in dwords, so 64-bit reads/writes need two
+ * 32-bit reads/writes
+ *
+ * x = 0-Fh
+ * y = 0,4,8,Ch
+ *
+ * FEC0 xy00h - Register select
+ * FEC0 xy10h - Data value
+ *
+ * REG SEL  - 32-bits - [31:8] Reserved - [7:0] - R/W
+ * DATA REG - 32-bits - All R/W
+ *
+ * Address offset is determined by register select bits [0:7]
+ *
+ * Registers:
+ * 0x00 - I/O APIC ID  - R/W
+ * 0x01 - I/O APIC VER - RO
+ * 0x02 - I/O APIC Arbitration ID - RO
+ * 0x10-0x3F - I/O Redirection table entries 0-23 64-bit each - R/W
+ *
+ * Registers:
+ * ID  - [31:28] Reserved - [27:24] ID - [23:0] Reserved
+ * VER - [31:24] Reserved - [23:16] Max Redirections - [15:8] Reserved - [7:0] Version
+ * ARB - [31:28] Reserved - [27:24] ID - [23:0] Reserved
+ *
+ * Redirection Registers:
+ * Interrupt priority is independent of the physical location (unlike the 8259A)
+ * Vector maps to priority, and each interrupt can be assigned a vector
+ *
+ * [63:56] - Destination field - When mode = physical then [59:56] = APIC ID
+ *                             - When mode = logical then [63:56] = Set of processors
+ * [55:17] - Reserved
+ * [16]    - Interrupt mask - 1 = masked - 0 = not masked
+ * [15]    - Trigger mode - 1 = level sensitive - 0 = edge sensitive
+ * [14]    - For level intrs only - Set to 1 when 
+ * [13]    - Pin polarity - 1 = active high - 0 = active low
+ * [12]    - Delivery status - 1 = send pending - 0 = idle
+ * [11]    - Destination Mode - 1 = logical mode - 0 = physical mode
+ * [10:8]  - Delivery mode - 000 = fixed mode
+ *                         - 001 = lowest priority
+ *                         - 010 = SMI - edge trigger required
+ *                         - 011 = Reserved
+ *                         - 100 = NMI - must be programmed as edge triggered
+ *                         - 101 = INIT - must be programmed as edge triggered
+ *                         - 110 = Reserved
+ *                         - 111 = ExtINT - must be programmed as edge triggered
+ * [7:0]   - Interrupt vector - Valid range is 0x10-0xFE
+ */
+void ioapic_write(uint32_t* addr, uint8_t reg_idx, uint32_t val)
+{
+	*addr = reg_idx;
+	*(addr+1) = val;
+}
 
-	kprintf("Found MP Sig: 0x%x\n", mfps);
-	kprintf("Sig: 0x%x\n", mfps->signature);
-	kprintf("PA Ptr: 0x%x\n", mfps->phys_addr_ptr);
-	kprintf("Len: %d\n", mfps->length);
-	kprintf("Spec Rev: %d\n", mfps->spec_rev);
-	kprintf("Checksum: 0x%x\n", mfps->checksum);
-	kprintf("Feature Info 1: 0x%x\n", mfps->feature_info1);
-	kprintf("Feature Info 2: 0x%x\n", mfps->feature_info2);
-
-	// If the phys_addr_ptr field is non-zero then there is a 
-	// configuration table present. Also if the feature_info1
-	// field is zero then an MP configuration table is present.
-	if (mfps->phys_addr_ptr != 0)
-	{
-		MPConfigTable* mct = (MPConfigTable*) (uint64_t) mfps->phys_addr_ptr;
-		kprintf("\n\nSig: 0x%x\n", mct->signature);
-		kprintf("Tbl Len: %d\n", mct->base_length);
-		kprintf("Spec Rev: %d\n", mct->spec_rev);
-		kprintf("Checksum: 0x%x\n", mct->checksum);
-		kprintf("OEM ID: %l8s\n", mct->oem_id);
-		kprintf("PROD ID: %l12s\n", mct->product_id);
-		kprintf("OEM Tbl Ptr: 0x%x\n", mct->oem_table_ptr);
-		kprintf("OEM Tbl Size: %d\n", mct->oem_table_size);
-		kprintf("Entry Count: %d\n", mct->entry_count);
-		kprintf("Apic Loc: 0x%x\n", mct->lapic_addr);
-		kprintf("Ext Tbl Len: %d\n", mct->ext_table_length);
-		kprintf("Ext Tbl Check: %d\n", mct->ext_table_checksum);
-
-		parse_mct(mct);
-	}
+uint32_t ioapic_read(uint32_t* addr, uint8_t reg_idx)
+{
+	*addr = reg_idx;
+	return *(addr+1);
 }
 
